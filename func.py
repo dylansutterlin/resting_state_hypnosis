96516ed29cd6539cb14159424184cb3e6da9a87f
dylansutterlin <dylan.sutterlin-guindon@umontreal.ca>
2023-05-26 16:15:09 -0400

    git push origin main ```

--- func.py
+++ func.py
@@ -2,7 +2,10 @@ import os
 import glob as glob
 import pandas as pd
 import numpy as np
+import nibabel as nib
 from sklearn.utils import Bunch
+from nilearn import plotting
+
 
 def load_data(path):
     """
@@ -10,32 +13,262 @@ def load_data(path):
 
     """
     data = Bunch(
+        subjects=[sub for sub in os.listdir(path) if "APM" in sub],
+        pre_hyp=[
+            glob.glob(os.path.join(path, sub, "*before*", "*4D*"))[0]
+            for sub in os.listdir(path)
+            if "APM" in sub
+        ],
+        post_hyp=[
+            glob.glob(os.path.join(path, sub, "*during*", "*4D*"))[0]
+            for sub in os.listdir(path)
+            if "APM" in sub
+        ],
+        anat=[
+            glob.glob(os.path.join(path, sub, "anatomy", "*.nii"))[0]
+            for sub in os.listdir(path)
+            if "APM" in sub
+        ],
+        phenotype=pd.DataFrame(
+            pd.read_excel(
+                glob.glob(os.path.join(path, "*variables*"))[0],
+                sheet_name=0,
+                index_col=1,
+                header=2,
+            )
+        ),
+    )
 
-        subjects = [sub for sub in os.listdir(path) if 'APM' in sub],
-        pre_hyp = [glob.glob(os.path.join(path, sub, '*before*', '*4D*'))[0] for sub in os.listdir(path) if 'APM' in sub],
-        post_hyp = [glob.glob(os.path.join(path, sub, '*during*', '*4D*'))[0] for sub in os.listdir(path) if 'APM' in sub],
-        anat = [glob.glob(os.path.join(path, sub, 'anatomy', '*.nii'))[0] for sub in os.listdir(path) if 'APM' in sub],
-        phenotype = pd.DataFrame(pd.read_excel(glob.glob(os.path.join(path, '*variables*'))[0], sheet_name = 0, index_col = 1, header = 2))
-        )
-    
     return data
 
-def diff_rz(pre_connectome, post_connectome, verbose = True):
-    '''
+
+def diff_rz(pre_connectome, post_connectome, verbose=True):
+    """
 
     pre (list) : list with all connectomes from pre condition
     post(list) : List with all connectomes from post condition
 
-    '''
+    """
 
     diff = list()
     for pre, post in zip(pre_connectome, post_connectome):
-        res  = np.arctanh(pre) - np.arctanh(post)
+        res = np.arctanh(pre) - np.arctanh(post)
         diff.append(res)
 
     if verbose:
-        print('Computing diff in lists of {}, {} connectome with r to Z arctanh func./n Diff matrix has shape : {} '.format(len(pre_connectome), len(post_connectome), diff[0].shape))
+        print(
+            "Computing diff in lists of {}, {} connectome with r to Z arctanh func./n Diff matrix has shape : {} ".format(
+                len(pre_connectome), len(post_connectome), diff[0].shape
+            )
+        )
 
     return diff
 
-#def reg_model(conne)
+
+def plot_bilat_nodes(correlation_matrix, atlas_file, atlas_name, reduce_roi=False):
+    if reduce_roi:
+        load_mask = nib.load(atlas_filename)
+        filt_mask = np.array(load_mask.dataobj)
+        filt_mask[filt_mask == 9.0] = 0  # 9. is the label of this ROI we are removing
+        atlas_file = filt_mask
+
+    left_coordinates = plotting.find_parcellation_cut_coords(labels_img=atlas_file)
+    right_coordinates = plotting.find_parcellation_cut_coords(
+        labels_img=atlas_file, label_hemisphere="right"
+    )
+
+    plot_connectome(
+        correlation_matrix,
+        left_coordinates,
+        right_coordinates,
+        edge_threshold=None,
+        title=atlas_name,
+    )
+    plotting.show()
+
+
+def sym_matrix_to_vec(symmetric, discard_diagonal=True):
+    """Return the flattened lower triangular part of an array.
+
+    If diagonal is kept, diagonal elements are divided by sqrt(2) to conserve
+    the norm. Acts on the last two dimensions of the array if not 2-dimensional.
+
+    Parameters
+    ----------
+    symmetric : numpy.ndarray or list of numpy arrays, shape\
+        (..., n_features, n_features)
+        Input array.
+
+    discard_diagonal : boolean, optional
+        If True, the values of the diagonal are not returned.
+        Default=False.
+
+    Returns
+    -------
+    output : numpy.ndarray
+        The output flattened lower triangular part of symmetric. Shape is
+        (..., n_features * (n_features + 1) / 2) if discard_diagonal is False
+        and (..., (n_features - 1) * n_features / 2) otherwise.
+
+    """
+    if discard_diagonal:
+        # No scaling, we directly return the values
+        tril_mask = np.tril(np.ones(symmetric.shape[-2:]), k=-1).astype(bool)
+        return symmetric[..., tril_mask]
+    scaling = np.ones(symmetric.shape[-2:])
+    np.fill_diagonal(scaling, sqrt(2.0))
+    tril_mask = np.tril(np.ones(symmetric.shape[-2:])).astype(bool)
+    return symmetric[..., tril_mask] / scaling[tril_mask]
+
+    # def reg_model(connectomes)
+
+    features = []
+
+
+from matplotlib import cm
+from nilearn.plotting import plot_glass_brain
+
+
+def plot_connectome(
+    adjacency_matrix,
+    node_coords_left,
+    node_coords_right,
+    node_color="auto",
+    node_size=50,
+    edge_cmap=cm.bwr,
+    edge_vmin=None,
+    edge_vmax=None,
+    edge_threshold=None,
+    output_file=None,
+    display_mode="ortho",
+    figure=None,
+    axes=None,
+    title=None,
+    annotate=True,
+    black_bg=False,
+    alpha=0.7,
+    edge_kwargs=None,
+    node_kwargs=None,
+    colorbar=False,
+):
+    """Plot connectome on top of the brain glass schematics.
+
+    The plotted image should be in MNI space for this function to work
+    properly.
+
+    In the case of 'l' and 'r' directions (for hemispheric projections),
+    markers in the coordinate x == 0 are included in both hemispheres.
+
+    Parameters
+    ----------
+    adjacency_matrix : numpy array of shape (n, n)
+        Represents the link strengths of the graph. The matrix can be
+        symmetric which will result in an undirected graph, or not
+        symmetric which will result in a directed graph.
+
+    node_coords : numpy array_like of shape (n, 3)
+        3d coordinates of the graph nodes in world space.
+
+    node_color : color or sequence of colors or 'auto', optional
+        Color(s) of the nodes. If string is given, all nodes
+        are plotted with same color given in string.
+
+    node_size : scalar or array_like, optional
+        Size(s) of the nodes in points^2. Default=50.
+
+    edge_cmap : colormap, optional
+        Colormap used for representing the strength of the edges.
+        Default=cm.bwr.
+
+    edge_vmin, edge_vmax : float, optional
+        If not None, either or both of these values will be used to
+        as the minimum and maximum values to color edges. If None are
+        supplied the maximum absolute value within the given threshold
+        will be used as minimum (multiplied by -1) and maximum
+        coloring levels.
+
+    edge_threshold : str or number, optional
+        If it is a number only the edges with a value greater than
+        edge_threshold will be shown.
+        If it is a string it must finish with a percent sign,
+        e.g. "25.3%%", and only the edges with a abs(value) above
+        the given percentile will be shown.
+    %(output_file)s
+    display_mode : string, optional
+        Choose the direction of the cuts: 'x' - sagittal, 'y' - coronal,
+        'z' - axial, 'l' - sagittal left hemisphere only,
+        'r' - sagittal right hemisphere only, 'ortho' - three cuts are
+        performed in orthogonal directions. Possible values are: 'ortho',
+        'x', 'y', 'z', 'xz', 'yx', 'yz', 'l', 'r', 'lr', 'lzr', 'lyr',
+        'lzry', 'lyrz'. Default='ortho'.
+    %(figure)s
+    %(axes)s
+    %(title)s
+    %(annotate)s
+    %(black_bg)s
+        Default=False.
+    alpha : float between 0 and 1, optional
+        Alpha transparency for the brain schematics. Default=0.7.
+
+    edge_kwargs : dict, optional
+        Will be passed as kwargs for each edge matlotlib Line2D.
+
+    node_kwargs : dict, optional
+        Will be passed as kwargs to the plt.scatter call that plots all
+        the nodes in one go.
+    %(colorbar)s
+        Default=False.
+
+    See Also
+    --------
+    nilearn.plotting.find_parcellation_cut_coords : Extraction of node
+        coords on brain parcellations.
+    nilearn.plotting.find_probabilistic_atlas_cut_coords : Extraction of
+        node coords on brain probabilistic atlases.
+
+    """
+    display = plot_glass_brain(
+        None,
+        display_mode=display_mode,
+        figure=figure,
+        axes=axes,
+        title=title,
+        annotate=annotate,
+        black_bg=black_bg,
+        alpha=alpha,
+    )
+
+    display.add_graph(
+        adjacency_matrix,
+        node_coords_left,
+        node_color=node_color,
+        node_size=node_size,
+        edge_cmap=edge_cmap,
+        edge_vmin=edge_vmin,
+        edge_vmax=edge_vmax,
+        edge_threshold=edge_threshold,
+        edge_kwargs=edge_kwargs,
+        node_kwargs=node_kwargs,
+        colorbar=colorbar,
+    )
+
+    display.add_graph(
+        adjacency_matrix,
+        node_coords_right,
+        node_color=node_color,
+        node_size=node_size,
+        edge_cmap=edge_cmap,
+        edge_vmin=edge_vmin,
+        edge_vmax=edge_vmax,
+        edge_threshold=edge_threshold,
+        edge_kwargs=edge_kwargs,
+        node_kwargs=node_kwargs,
+        colorbar=colorbar,
+    )
+
+    if output_file is not None:
+        display.savefig(output_file)
+        display.close()
+        display = None
+
+    return display
--- main_con.py
+++ main_con.py
@@ -8,38 +8,64 @@ import nibabel as nib
 from nilearn.maskers import NiftiMapsMasker, NiftiLabelsMasker, MultiNiftiLabelsMasker, MultiNiftiMapsMasker
 from nilearn.connectome import ConnectivityMeasure
 import func
-from nilearn import datasets, plotting
+from nilearn import datasets, plotting, image
 from nilearn.regions import connected_label_regions
 
 
-def con_matrix(data_dir, save_to, path_to_atlas = None, atlas_type = 'labels', connectivity_measure = 'correlation', con_stat = 'diff', plot_atlas = False, verbose = True):
-    """
-    Computes connectivity matrices for many subjects.
+def con_matrix(data_dir, save_to, path_to_atlas = None, atlas_type = 'labels', connectivity_measure = 'correlation', plot_atlas = False, verbose = False):
+    """_summary_
 
-
+    Parameters
+    ----------
+    data_dir : str
+        Path to data
+    save_to : str
+        Path to saving folder
+    path_to_atlas : _type_, optional
+        Path to atlas, by default None
+    atlas_type : str, optional
+        Choices : 'labels' and 'maps' for probabilistic atlases, by default 'labels'
+    connectivity_measure : str, optional
+        Correlation estimation, by default 'correlation'
+    plot_atlas : bool, optional
+        by default False
+    verbose : bool, optional
+        Wether to print/plot outputs, by default False
     """
 
 
     # --Data--
     data = func.load_data(data_dir)
-    idcs = [i + 1 for i in range(0,len(data.subjects))]
-    conditions = ['pre_hyp', 'post_hyp', 'diff']
+    conditions = ['pre_hyp', 'post_hyp', 'contrast']
     pre_data = data.pre_hyp
     post_data = data.post_hyp
-    print(pre_data)
     results = dict(pre_series = list(), post_series =  list())
     Y = data.phenotype['PercDiffAutomat'][2:-2].T[1]
 
     # --Atlas choices--
+    atlas, atlas_filename, atlas_labels = load_choose_atlas('')
     #atlas = datasets.fetch_atlas_msdl()
     atlas = datasets.fetch_atlas_basc_multiscale_2015(version="sym", resolution=64) #, resolution=None)
     #dataset = datasets.fetch_atlas_harvard_oxford('cort-maxprob-thr25-2mm')
-    atlas_filename = datasets.fetch_atlas_yeo_2011()['thick_7']
-    atlas_name = 'yeo_7'
+    atlas_filename = datasets.fetch_atlas_yeo_2011()['thick_17']
+    atlas_name = 'yeo_17'
     atlas_labels  = ['Visual', 'Somatosensory', 'Dorsal Attention', 'Ventral Attention', 'Limbic', 'Frontoparietal', 'Default']
-    print(nib.load(atlas_filename).shape)
-    from nilearn import plotting
-    plotting.plot_roi(atlas_filename, title=atlas_name)
+
+    if atlas_name == 'yeo_17':
+
+        # Missing ROIs correction
+        load_mask = nib.load(atlas_filename)
+        filt_mask = np.array(load_mask.dataobj)
+        filt_mask[filt_mask == 9.] = 0 # 9. is the label of this ROI we are removing
+
+        # making ROIs bilateral
+        mask_data_right[mask_data_right>0] += mask_data.max()
+        bilateral_mask_data = mask_data_left + mask_data_right
+
+        atlas_labels = [str(int(x)) for x in np.unique(filt_mask)][1:] # Remove 1st as it is the background
+        atlas_filename = image.new_img_like(load_mask, filt_mask)
+
+
     # --Labels--
     #region_labels = connected_label_regions(atlas)
 
@@ -67,54 +93,67 @@ def con_matrix(data_dir, save_to, path_to_atlas = None, atlas_type = 'labels', c
 
    # --Fit and apply mask--
     #conf, sample_mask = load_confounds_strategy(func_path, denoise_strategy = 'simple', motion = 'basic', global_signal = 'basic')
-    masker.fit(pre_data)
-    results['pre_series'] = [masker.transform(ts) for ts in pre_data] # Confounds?
-    masker.fit(post_data)
-    results['post_series'] =  [masker.transform(ts) for ts in post_data] # A list of time series for each sub
-    print(results['post_series'][0].shape)
+    #masker.fit(pre_data)
+    results['pre_series'] = [masker.fit_transform(ts) for ts in pre_data] # Confounds?
+    #masker.fit(post_data)
+    results['post_series'] =  [masker.fit_transform(ts) for ts in post_data] # A list of time series for each sub
+    print([ts.shape for ts in results['pre_series']])
+    print([ts.shape for ts in results['post_series']])
+
+    # Filter TS !!!
+    #for i, shapes, sub in enumerate()
+    #idcs = [idcs for idcs, index in enumerate(data.phenotype.index) if index in rename_sub] # Check indices of Y[i] of sub included in analysis
+    #y_auto = np.array(y_full_auto[idcs])
+
     # -- Covariance Estimation--
     correlation_measure = ConnectivityMeasure(kind=connectivity_measure,  discard_diagonal=True)
+
     results['pre_connectomes'] = correlation_measure.fit_transform(results['pre_series'])
-    results['pre_mean_connetomes'] = correlation_measure.mean_
+    results['pre_mean_connectome'] = correlation_measure.mean_
     results['post_connectomes'] =  correlation_measure.fit_transform(results['post_series'])
-    results['post_mean_connetomes'] = correlation_measure.mean_
-    #results['diff_mean_connectomes'] =  func.diff_rz(results['pre_connectomes'], results['post_connectomes'])
-    results['diff_connectomes'] = [post - pre for post, pre in zip(results['post_mean_connetomes'],results['pre_mean_connetomes'])]
-    print(results['pre_connectomes'][0].shape, len(results['pre_connectomes']))
+    results['post_mean_connectome'] = correlation_measure.mean_
+    results['contrast_mean_connectome'] =  results['post_mean_connectome'] - results['pre_mean_connectome']
+    results['contrast_connectomes'] = [post - pre for post, pre in zip(results['post_connectomes'],results['pre_connectomes'])]
+
     # --Save--
     for idx, sub in enumerate(data.subjects):
         np.save(os.path.join(save_to, f'{sub}_{conditions[0]}_connectomes'), results['pre_connectomes'][idx], allow_pickle=True)
         np.save(os.path.join(save_to, f'{sub}_{conditions[1]}_connectomes'), results['post_connectomes'][idx], allow_pickle=True)
-        np.save(os.path.join(save_to, f'{sub}_{conditions[2]}_connectomes'), results['diff_connectomes'][idx], allow_pickle=True)
+        np.save(os.path.join(save_to, f'{sub}_{conditions[2]}_connectomes'), results['contrast_connectomes'][idx], allow_pickle=True)
+    np.save(os.path.join(save_to, f'{conditions[0]}_mean_connectome'), results['pre_mean_connectome'], allow_pickle=True)
+    np.save(os.path.join(save_to, f'{conditions[1]}_mean_connectome'), results['post_mean_connectome'], allow_pickle=True)
+    np.save(os.path.join(save_to, f'{conditions[2]}_mean_connectome'), results['contrast_mean_connectome'], allow_pickle=True)
 
 
     # --Stats--
-    features = np.asarray([np.load(os.path.join(save_to, f'{sub}_{conditions[2]}_connectomes.npy'), allow_pickle=True) for sub in data.subjects])
-    Y = data.phenotype['PercDiffAutomat']
-    #! pheno_df = data.phenotypic
-    # Best way to format data?
+    matrices = np.asarray([np.load(os.path.join(save_to, f'{sub}_{conditions[2]}_connectomes.npy'), allow_pickle=True) for sub in data.subjects])
+    y_full_auto = data.phenotype['Unnamed: 68'] # abs. diff. in perceived automaticity
+    # Access selected sub based on id in y
+    rename_sub = [f'APM{num}' for num in [sub[4:6] for sub in data.subjects]] # Will rename 'APM_01_H1' with 'APM01'
+    idcs = [idcs for idcs, index in enumerate(data.phenotype.index) if index in rename_sub] # Check indices of Y[i] of sub included in analysis
+    y_auto = np.array(y_full_auto[idcs])
 
+    # --X/features (vectorize each connectome)--
+    tril_mask = np.tril(np.ones(results['pre_connectomes'].shape[-2:]), k=-1).astype(bool) # ! assuming same shape for other matrices (post, and contrast)
+    results['preX'] = np.stack([results['pre_connectomes'][i][..., tril_mask] for i in range(0,len(results['pre_connectomes']))], axis = 0)
+    results['postX'] = np.stack([results['post_connectomes'][i][..., tril_mask] for i in range(0,len(results['post_connectomes']))], axis = 0)
+    results['contrastX'] = np.stack([results['contrast_connectomes'][i][..., tril_mask] for i in range(0,len(results['contrast_connectomes']))], axis = 0)
+
+    np.save(os.path.join(save_to, f'features_pre'), results['preX'], allow_pickle=True)
+    np.save(os.path.join(save_to, f'features_post'), results['postX'], allow_pickle=True)
+    np.save(os.path.join(save_to, f'features_contrast'), results['contrastX'], allow_pickle=True)
+    np.save(os.path.join(save_to, f'Y'), y_auto, allow_pickle=True)
+
+
+    # --Prints and plot--
     if verbose:
-        # Mask out the major diagonal
-        for correlation_matrix in [results['pre_connectomes'][0], results['post_connectomes'][0], results['diff_connectomes']]:#[results['pre_mean_connetomes'], results['post_mean_connetomes']]:
+
+        for correlation_matrix in [results['pre_mean_connectome'], results['post_mean_connectome'], results['contrast_mean_connectome']]:#[results['pre_mean_connetomes'], results['post_mean_connetomes']]:
             np.fill_diagonal(correlation_matrix, 0)
-            print(correlation_matrix.shape)
-            print(atlas_labels)
-            plotting.plot_matrix(
-                correlation_matrix, labels=atlas_labels, colorbar=True, vmax=0.8, vmin=-0.8
-            )
+            plotting.plot_matrix(correlation_matrix, labels=atlas_labels, colorbar=True, vmax=0.8, vmin=-0.8)
+            func.plot_bilat_nodes(correlation_matrix, atlas_filename, atlas_name)
 
-            coordinates = plotting.find_parcellation_cut_coords(labels_img=atlas_filename)
-            plotting.plot_connectome(
-                correlation_matrix,
-                coordinates,
-                edge_threshold=None,
-                title= atlas_name,
-            )
-            plotting.show()
-
-
-
+        plotting.plot_roi(atlas_filename, title=atlas_name)
 '''
 # plot connectivity matrix
 
@@ -124,6 +163,6 @@ def con_matrix(data_dir, save_to, path_to_atlas = None, atlas_type = 'labels', c
     plt.clf()
 '''
 
-p = r'E:\Users\Dylan\Desktop\UdeM_H22\E_PSY3008\data_desmartaux\test_data_ASL'
-save_to = r'C:\Users\Dylan\Desktop\UM_Bsc_neurocog\E22\Projet_Ivado_rainvillelab\results\test_connec'
-con_matrix(p, save_to, path_to_atlas = datasets.fetch_atlas_basc_multiscale_2015())
+p = r'E:\Users\Dylan\Desktop\UdeM_H22\E_PSY3008\data_desmartaux\HYPNOSIS_ASL_DATA'
+save_to = r'C:\Users\Dylan\Desktop\UM_Bsc_neurocog\E22\Projet_Ivado_rainvillelab\results\partial_connect_hyp_yeo17thick'
+con_matrix(p, save_to, path_to_atlas = datasets.fetch_atlas_basc_multiscale_2015(), connectivity_measure = 'partial correlation')
